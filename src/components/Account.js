bf9408ecd0f32f9b09b701dd2f6bda27b8a7a136
mahadia-islam <74854813+mahadia-islam@users.noreply.github.com>
2021-09-06 14:49:35 +0600

    new changed

---
+++ src/components/Account.js
@@ -0,0 +1,13 @@
+function Account() {
+    return (
+        <div class="account">
+            <span class="material-icons-outlined" title="Account">
+                account_circle
+            </span>
+            <a href="signup.html">Signup</a>
+            <span class="material-icons-outlined" title="Logout"> logout </span>
+        </div>
+    )
+}
+
+export default Account;
